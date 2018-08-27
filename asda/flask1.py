be8b90a184291f26ea51800275fd21c8e5261263
zhangsanfeng2018 <484437848@qq.com>
2018-08-27 17:19:07 +0800

    sdfhkjas

---
+++ asda/flask1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 16:48:09 2018
+
+@author: ASUS
+"""
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'hello'
+@app.route('/user/<name>')
+def user(name):
+    return 'hello,%s' % name
+if __name__ == '__mian__':
+    app.run()
+    
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return 'hello'
+if __name__ == '__name__':
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
